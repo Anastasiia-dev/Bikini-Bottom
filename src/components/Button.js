20645a6861ecc88df1bf7a5ca19d903c82bacbc4
Alexey Yudin <alexey.y.yudin@gmail.com>
2021-01-20 17:18:44 +0100

    prettier

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Button.css";
 
-
-function Button(){
-    return(
-        <>
-            <button className="btn-learn-more" href='#'>LEARN MORE</button>
-            <button className="btn-youtube" href='#'>WATCH</button>
-        </>
-    )
-    
+function Button() {
+  return (
+    <>
+      <button className="btn-learn-more" href="#">
+        LEARN MORE
+      </button>
+      <button className="btn-youtube" href="#">
+        WATCH
+      </button>
+    </>
+  );
 }
 
 export default Button;
